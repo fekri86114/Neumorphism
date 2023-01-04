4957d8878947ceaf5d9d683465413d290d2263f1
root <fekri.mohammadreza86@gmail.com>
2023-01-04 13:13:12 +0330

    init commit

---
+++ app/src/main/java/fekri/io/MainActivity.kt
@@ -0,0 +1,21 @@
+package fekri.io
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import fekri.io.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMessage.setOnClickListener {
+            Toast.makeText(this, "Hooray!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
